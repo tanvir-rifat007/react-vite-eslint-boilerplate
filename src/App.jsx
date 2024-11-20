f51589108c8684ea8c22edd90764c93da33756f4
tanvir07-cell <tr9836859@gmail.com>
2024-11-20 00:01:08 -0800

    Eslint,vite,prettier,react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(<App />);
